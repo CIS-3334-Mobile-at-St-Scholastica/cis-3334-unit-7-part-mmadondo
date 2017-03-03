0508d49c755dc332e7d1b538cbf36caf01f9b5b7
mmadondo <madondomalvern@gmail.com>
2017-03-03 15:57:42 -0600

    Class Activity By Malvern and Evan

--- app/src/main/java/css/cis3334/heartratetracker/MainActivity.java
+++ app/src/main/java/css/cis3334/heartratetracker/MainActivity.java
@@ -8,16 +8,18 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Random;
 /**
  * Created by Tom Gibbons in Feb 2017.
+ * Revised by Malvern Madondo and Evan Mumbleau
  * For the CIS 3334 class at St. Scholastica
  */
 
-public class MainActivity extends AppCompatActivity implements Serializable {
+public class MainActivity extends AppCompatActivity {
 
     ListView lvHeartRates;
     TextView tvSelect;
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     //ArrayList<HeartRate> basicheartRateList;
 
     // constant to determine which sub-activity returns
-    private static final int HR_REQUEST_CODE = 1001;
+   // private static final int HR_REQUEST_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,14 +52,20 @@ public class MainActivity extends AppCompatActivity implements Serializable {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
                 HeartRate hr = (HeartRate) parent.getItemAtPosition(position);
-                tvSelect.setText("You selected: " + hr.toString());
-                String range = hr.getRangeName().toString();
+              //  tvSelect.setText("You selected: " + hr.toString());
+
                 Double pulse = Double.parseDouble(hr.getPulse().toString());
-                Intent secActIntent = new Intent(MainActivity.this, Main2Activity.class);
+                String range = hr.getRange().toString();
+
+                //when the user selects a heartrate from the list, starts new activity using an intent
+                Intent newIntent = new Intent(MainActivity.this, Main2Activity.class);
+
+                //passing selected heart rate obkect from main to the new activity
+                newIntent.putExtra("Pulse", pulse);
+                newIntent.putExtra("Range", range);
+
                 //startActivityForResult(secActIntent, HR_REQUEST_CODE);
-                secActIntent.putExtra("Pulse", pulse);
-                secActIntent.putExtra("Range", range);
-                startActivity(secActIntent);
+                startActivity(newIntent);
 
             }
         });
